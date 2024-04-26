af3be08248cc32c174f8ea1d8c73ba0da490a42d
Cole <cac2107@rit.edu>
2024-04-26 15:09:10 -0400

    Updated

--- deliverable/orbt.py
+++ deliverable/orbt.py
@@ -4,22 +4,21 @@ import os
 from tqdm import tqdm
 
 def detect(detector, image):
-    finger = cv2.imread(image)
-    finger = cv2.cvtColor(finger, cv2.COLOR_BGR2RGB)
-    key_points, des = detector.detectAndCompute(finger, None)
-    return finger, key_points, des
+    f_print = cv2.imread(image)
+    f_print = cv2.cvtColor(f_print, cv2.COLOR_BGR2RGB)
+    key_points, des = detector.detectAndCompute(f_print, None)
+    return f_print, key_points, des
 
 def compare_prints(image1, image2):
     detector = cv2.ORB_create() 
-    fng1, kp1, des1 = detect(detector, image1)
-    fng2, kp2, des2 = detect(detector, image2)
+    _, _, des1 = detect(detector, image1)
+    _, _, des2 = detect(detector, image2)
   
     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
     matches = bf.match(des1, des2)
    
     count = 0 
-    for i in matches:
-        count += 1
+    for _ in matches: count += 1
 
     return count/500
 
